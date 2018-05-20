e08078384d89cf7e7b98ae5334a86de8726a0831
ben <bag131@txstate.edu>
2018-05-20 15:15:56 -0500

    updated linear regression script

--- Py_torch_practice/logisitic_regression.py
+++ Py_torch_practice/logisitic_regression.py
@@ -5,37 +5,33 @@ import torchvision.datasets as datasets
 from torch.autograd import Variable
 import numpy as np
 
-class LinearRegressionModel(nn.Module):
-    def __init__(self, input_size, output_size):
+class LogisticRegression(nn.Module):
+    def __init__(self, input_size):
         super().__init__()
         #Looks like this is the first layer
-        #and the only layer 
-        self.linear = nn.Linear(input_size, output_size)
+        #and the only layer other than an
+        #output layer which manipulates the output
+        self.linear = nn.LogSoftmax(input_size)
 
     def forward(self, x):
         out = self.linear(x)
         return out
 
-x_values = [i for i in range(10)]
+x_values = [i for i in range(11)]
 
 x_train = np.array(x_values, dtype=np.float32)
 
-print(x_train)
-x_train =x_train.reshape(5,2)
+x_train =x_train.reshape(-1,1)
 
-print(x_train)
-y_values = [4*i +1 for i in range(5) ]
+y_values = [2*i +1 for i in x_values ]
 
 y_train = np.array(y_values, dtype=np.float32)
 
-y_train = y_train.reshape(5,1)
+y_train = y_train.reshape(-1,1)
+input_size = 1
+output_size = 1
 
-print(y_train.shape[1])
-print(y_train)
-input_size = 2 ## this is the number of x values i.e y = x0 + x1m +x2m
-output_size = 1 # dependent variable. only one y per function of y = x0 + x1m....
-
-model = LinearRegressionModel(input_size, output_size)
+model = LogisticRegression(input_size)
 
 evaluater = nn.MSELoss()
 
@@ -43,11 +39,10 @@ learning_rate = 0.01
 
 weight_updater = torch.optim.SGD(model.parameters(),lr=learning_rate)
 
-inputs = Variable(torch.from_numpy(x_train))
-GT = Variable(torch.from_numpy(y_train))
-
-
 for epoch in range(100):
+    
+    inputs = Variable(torch.from_numpy(x_train))
+    GT = Variable(torch.from_numpy(y_train))
 
     weight_updater.zero_grad()
 
