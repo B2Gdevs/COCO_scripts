7d79d3f84bcbc31a012a961c64852ceb023f7572
ben <bag131@txstate.edu>
2018-05-20 21:19:10 -0500

    finally figured out the cross entropy.

--- Py_torch_practice/logisitic_regression.py
+++ Py_torch_practice/logisitic_regression.py
@@ -5,58 +5,126 @@ import torchvision.datasets as datasets
 from torch.autograd import Variable
 import numpy as np
 
-class LogisticRegression(nn.Module):
-    def __init__(self, input_size):
+class LinearRegressionModel(nn.Module):
+    def __init__(self, input_size, output_size):
         super().__init__()
         #Looks like this is the first layer
         #and the only layer other than an
         #output layer which manipulates the output
-        self.linear = nn.LogSoftmax(input_size)
+        self.linear = nn.Linear(input_size, output_size)
 
     def forward(self, x):
         out = self.linear(x)
         return out
 
-x_values = [i for i in range(11)]
+'''
+these x values have 2 dimensions that are fed.  These would 
+be the x values in y = x0 + x1m1 + x2m2....
 
+Since we are predicting 11 classes this needs to also be
+11 rows long
+'''
+x_values = [[i,i] for i in range(11)]
+
+'''
+the cross entropy loss function takes a float32 tensor and
+a long tensor.  Float is for the x_values that will be passed
+to the linear regression model and then passed to 
+the loss function.
+'''
 x_train = np.array(x_values, dtype=np.float32)
+labels = Variable(torch.LongTensor([0,1,2,3,4,5,6,7,8,9,10]))
 
-x_train =x_train.reshape(-1,1)
+'''
+input size is how many x values we are saying will come
+y = x0 + x1m1 + x2m2.....
 
-y_values = [2*i +1 for i in x_values ]
+the x values being the amount of x's supplied.  However I 
+do not believe my example is completely accurate of what's
+going on in this case.  
 
-y_train = np.array(y_values, dtype=np.float32)
+The output size is the amount of predictions to be made.  This
+must be the same amount of classes since the cross entropy
+function will give a probability for each class. 
+'''
+input_size = 2
+output_size = 11
 
-y_train = y_train.reshape(-1,1)
-input_size = 1
-output_size = 1
-
-model = LogisticRegression(input_size)
-
-evaluater = nn.MSELoss()
+model = LinearRegressionModel(input_size, output_size)
+evaluater = nn.CrossEntropyLoss()
 
 learning_rate = 0.01
-
 weight_updater = torch.optim.SGD(model.parameters(),lr=learning_rate)
+    
+'''
+Since we are using a Variable class from autograd, it looks
+like we are passing nothing to other parts of the code.  This
+is true for us programmers just using pytorch.  However, 
+the data is actually being passed to each other by way of 
+a graph being made in the background.  Hence the name autograd,
+it automatically builds graphs and gradients as long as we 
+state which variables should be on it.
+'''
+inputs = Variable(torch.from_numpy(x_train)) 
 
 for epoch in range(100):
-    
-    inputs = Variable(torch.from_numpy(x_train))
-    GT = Variable(torch.from_numpy(y_train))
 
     weight_updater.zero_grad()
 
     outputs = model(inputs)
-    loss = evaluater(outputs, GT)
+
+    print(outputs)
+    loss = evaluater(outputs, labels)
 
     loss.backward()
+    
+    '''
+    Right here is what I was touching on about the Variables.
 
+    How does this thing update the weights if they are never
+    supplied to it?  Because we are using the graph and this
+    already has reference to those weights on that graph when
+    it was created.
+    '''
     weight_updater.step()
 
     print("epoch {}, loss {}".format(epoch, loss.data[0]))
 
+print(x_train)
 
-#lets get predicted values
 predictions = model(Variable(torch.from_numpy(x_train))).data.numpy()
 
+
+'''
+These predictions will look crazy. However the position of the
+highest number is the what the model predicted the class to be
+for the given 2 x values in out x_train list.  which was 11 x 2
+11x2 being that there are 11 entries of 2 x values.
+
+There will be 11 predictions for each row in the 11x2 matrix
+
+Choose the highest of the 11 predictions and the label at that
+position is the real prediction by the model
+
+Continue to look at the other 10 OUTPUTS and each of there
+11 predictions
+'''
 print(predictions)
+
+'''
+an example of another inference prediction when supplied
+with 2 x values.  The model I believe is off by about 2.5
+since the loss is also around 2.5.  This should have said 
+it was closer to 3rd or 2nd position however it was 
+highest at the 6 position.
+
+This is saying that given x values of [1,2] the prediction
+is 5.
+
+however this was trained saying that x values of [5,5] 
+should be 5.
+
+This is showing that the model is poor at predicting this 
+label
+'''
+print(model(Variable(torch.Tensor([1,2]))).data.numpy())
